395f6463b192aa344035c55036b8c4ed7d663497
jw <jerrioh@gmail.com>
2019-08-26 01:37:19 +0900

    번역, 로딩, 명언

--- app/src/main/java/org/jerrioh/diary/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.jerrioh.diary.config;
-
-public class Constants {
-
-    public static final String DEFAULT_TITLE = "NO TITLE";
-    public static final int PROHIBIT_DIARY_WRITE_HHMMSS = 10000;
-}
